3764c54b47887d76fffc0aafae0f42a1f2a33a16
Jayanth-Vegesna <jayanthvegesna@gmail.com>
2024-03-19 22:01:43 +0530

    Create 8_Type.py

---
+++ 8_Type.py
@@ -0,0 +1,24 @@
+# Type function
+a = 10          # int
+print(type(a))
+b = 10.5        # float
+print(type(b))
+c = 2+4j        # complex
+print(type(c))
+d = True        # bool
+print(type(d))
+e = None        # NoneType
+print(type(e))
+f = "hi"        # str
+print(type(f))
+g = [10,20,30]  # list
+print(type(g))
+h = (10,20,30)  # tuple (imputable)
+print(type(h))
+i = {10,20,30}  # set
+print(type(i))
+j = {10:"hi", 20:"hello"} # dict
+print(type(j))
+
+
+
