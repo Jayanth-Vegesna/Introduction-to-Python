68e20c830d4c72b064ae398100a7fab9c88b6a5c
Jayanth-Vegesna <127299837+Jayanth-Vegesna@users.noreply.github.com>
2024-04-04 22:32:14 +0530

    Add files via upload

---
+++ 21_Arithmetic_Progression.py
@@ -0,0 +1,6 @@
+print("Enter the following values of the Arithmatic Progression to find nth term")
+print("The formula for the nth term in an Arithmatic Progression is nth term = a +d(n-1)")
+a = int(input("Enter the first term: "))
+d = int(input("Enter the common difference(d): "))
+n = int(input("Enter the nth term: "))
+print("The value of nth term is: ", a+d*(n-1))
